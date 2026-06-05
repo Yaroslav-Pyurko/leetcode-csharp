1c9518d4082cc7d650c89d76e74ed094b3f2b85f
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-05 23:46:03 +0300

    feat: add Fibonacci number solutions

---
+++ src/LeetCode/01_Arrays/LC0509_FibonacciNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._01_Arrays
+{
+    public class LC0509_FibonacciNumber
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
