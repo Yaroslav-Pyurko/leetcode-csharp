dbd64719cf8975590a944dd2cf170b3d97b316e0
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-09 23:25:27 +0300

    refactor: rename solution files, fixed typos

--- tests/LeetCode.Tests/LC0001_TwoSumTests.cs
+++ tests/LeetCode.Tests/LC0001_TwoSumTests.cs
@@ -2,15 +2,13 @@
 
 namespace LeetCode.Arrays.Tests
 {
-    public class LC0001_TwoSum_Tests
+    public class LC0001_TwoSumTests
     {
         [Fact]
         public void Case1()
         {
             var s = new LC0001_TwoSum();
-
             var res = s.TwoSum([2, 7, 11, 15], 9);
-
             Assert.Equal(new[] { 0, 1 }, res);
         }
     }
