a5a5dcc803f6e710f170d3cb463af188e5e41578
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-11 00:01:08 +0300

    feat: solution Longest Common Prefix

---
+++ src/LeetCode/01_Arrays/LC0014_LongestCommonPrefix.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Arrays
+{
+    public class LC0014_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0 || strs.Any(x => string.IsNullOrWhiteSpace(x)))
+            {
+                return string.Empty;
+            }
+
+            Array.Sort(strs);
+            string first = strs[0];
+            string last = strs[strs.Length - 1];
+            int i = 0;
+            while (i < first.Length && i < last.Length)
+            {
+                if (first[i] == last[i])
+                    i++;
+                else break;
+            }
+            return first.Substring(0, i);
+        }
+    }
+}
