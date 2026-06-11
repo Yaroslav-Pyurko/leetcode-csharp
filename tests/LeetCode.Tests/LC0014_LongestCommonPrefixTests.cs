0b359dabf58b4f45979f2db30359d0491f7fbea4
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-11 21:23:13 +0300

    feat: add Longest Common Prefix tests

---
+++ tests/LeetCode.Tests/LC0014_LongestCommonPrefixTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Arrays;
+
+namespace LeetCode.Arrays.Tests
+{
+    public class LC0014_LongestCommonPrefixTests
+    {
+        [Fact]
+        public void Case1()
+        {
+            var solution = new LC0014_LongestCommonPrefix();
+            var actual = solution.LongestCommonPrefix(["flower", "flow", "flight"]);
+            var expected = "fl";
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            var solution = new LC0014_LongestCommonPrefix();
+            var actual = solution.LongestCommonPrefix(["dog", "racecar", "car"]);
+            var expected = "";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
