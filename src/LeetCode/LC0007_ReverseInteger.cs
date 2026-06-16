de025d686bd7119571393addda3cac001644e47a
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-16 15:11:02 +0300

    feat: add ReverseInteger v1.0 (math Revert) and tests

---
+++ src/LeetCode/LC0007_ReverseInteger.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode
+{
+    public class LC0007_ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var result = 0;
+
+            while (x != 0)
+            {
+                var lastDigit = x % 10;
+                var temp = result * 10 + lastDigit; // Add last digit to Result
+
+                // In case of overflow, the current value will not be equal to the previous one
+                var preResult = (temp - lastDigit) / 10;
+                if (preResult != result)
+                {
+                    return 0;
+                }
+
+                result = temp;
+                x /= 10; // next digit
+            }
+            return result;
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC0007_ReverseIntegerTests.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0007_ReverseIntegerTests
+    {
+        [Fact]
+        public void Case1()
+        {
+            var solution = new LC0007_ReverseInteger();
+            var actual = solution.Reverse(123);
+            var expected = 321;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            var solution = new LC0007_ReverseInteger();
+            var actual = solution.Reverse(-123);
+            var expected = -321;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case3()
+        {
+            var solution = new LC0007_ReverseInteger();
+            var actual = solution.Reverse(120);
+            var expected = 21;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
