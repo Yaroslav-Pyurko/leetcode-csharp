93810053e18f5384e48de343c58a6085473a2a2c
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-18 23:59:57 +0300

    feat: add Valid Parentheses and tests

---
+++ src/LeetCode/LC0020_ValidParentheses.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode
+{
+    public class LC0020_ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            var chars = s.ToCharArray();
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var curChat = chars[i];
+                if (curChat == '(' || curChat == '{' || curChat == '[')
+                {
+                    stack.Push(curChat);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    var lastChar = stack.Peek();
+                    if (curChat == ')' && lastChar == '(' ||
+                        curChat == '}' && lastChar == '{' ||
+                        curChat == ']' && lastChar == '['
+                        )
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC0020_ValidParenthesesTests.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0020_ValidParenthesesTests
+    {
+        [Fact]
+        public void Case1()
+        {
+            var solution = new LC0020_ValidParentheses();
+            var actual = solution.IsValid("()");
+            var expected = true;
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            var solution = new LC0020_ValidParentheses();
+            var actual = solution.IsValid("()[]{}");
+            var expected = true;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
+
+/*
+
+Input: s = "(]"
+Output: false
+
+Input: s = "([])"
+Output: true
+
+Input: s = "([)]"
+Output: false
+*/
