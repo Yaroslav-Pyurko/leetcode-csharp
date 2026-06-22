ef52700a78480e1373d50d9f13eb2e862fc32f69
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-22 23:52:24 +0300

    feat: add Remove Duplicates From Sorted Array and tests

---
+++ src/LeetCode/LC0026_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode
+{
+    public class LC0026_RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var j = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[j] = nums[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC0026_RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0026_RemoveDuplicatesFromSortedArrayTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        public void RemoveDuplicates_ReturnsExpectedResult(int[] input, int expected)
+        {
+            var solution = new LC0026_RemoveDuplicatesFromSortedArray();
+            var actual = solution.RemoveDuplicates(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
