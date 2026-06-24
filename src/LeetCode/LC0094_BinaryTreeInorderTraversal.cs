131bef615c934b6d7e85a095ba445dd332d77544
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-24 23:20:49 +0300

    feat: add Binary Tree Inorder Traversal

---
+++ src/LeetCode/LC0094_BinaryTreeInorderTraversal.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode
+{
+    public class LC0094_BinaryTreeInorderTraversal
+    {
+        IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            inOrder(root, ref result);
+            return result;
+        }
+
+        public void inOrder(TreeNode? node, ref List<int> result)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            inOrder(node.left, ref result);
+            result.Add(node.val);
+            inOrder(node.right, ref result);
+        }
+
+        public class TreeNode
+        {
+            public int val { get; set; }
+            public TreeNode? left { get; set; }
+            public TreeNode? right { get; set; }
+        }
+    }
+}
