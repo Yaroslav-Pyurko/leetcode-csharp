436e2c1fe72a4559494d908906d8013fb2759264
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-26 00:12:09 +0300

    feat: add Binary Tree Preorder Traversal

---
+++ src/LeetCode/LC0144_BinaryTreePreorderTraversal.cs
@@ -0,0 +1,26 @@
+﻿using static LeetCode.LC0094_BinaryTreeInorderTraversal;
+
+namespace LeetCode
+{
+    public class LC0144_BinaryTreePreorderTraversal
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+            preOrder(root, ref result);
+            return result;
+        }
+
+        private void preOrder(TreeNode node, ref List<int> result)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            result.Add(node.val);
+            preOrder(node.left, ref result);
+            preOrder(node.right, ref result);
+        }
+    }
+}
