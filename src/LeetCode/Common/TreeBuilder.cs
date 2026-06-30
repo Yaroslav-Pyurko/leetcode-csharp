d16530c0851dbf85a35cf0b5fb4813a49c3bbc44
Yaroslav Pyurko <yarickpy@gmail.com>
2026-06-30 21:52:20 +0300

    feat: add TreeBuilder

---
+++ src/LeetCode/Common/TreeBuilder.cs
@@ -0,0 +1,44 @@
+﻿using LeetCode;
+using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Common
+{
+    public static class TreeBuilder
+    {
+        public static TreeNode? Build(params int?[] values)
+        {
+            if (values.Length == 0 || values[0] == null)
+                return null;
+
+            var root = new TreeNode(values[0]!.Value);
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            int i = 1;
+
+            while (queue.Count > 0 && i < values.Length)
+            {
+                var node = queue.Dequeue();
+
+                if (i < values.Length && values[i] != null)
+                {
+                    node.left = new TreeNode(values[i]!.Value);
+                    queue.Enqueue(node.left);
+                }
+                i++;
+
+                if (i < values.Length && values[i] != null)
+                {
+                    node.right = new TreeNode(values[i]!.Value);
+                    queue.Enqueue(node.right);
+                }
+                i++;
+            }
+
+            return root;
+        }
+    }
+}
