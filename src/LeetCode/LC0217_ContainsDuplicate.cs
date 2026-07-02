5bf9d483f22f9343056cd582bd67ce7576d22703
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-02 23:28:52 +0300

    feat: add Contains Duplicate

---
+++ src/LeetCode/LC0217_ContainsDuplicate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class LC0217_ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var nums2 = new List<int>();
+            foreach (var n in nums)
+            {
+                if (nums2.Contains(n))
+                {
+                    return true;
+                }
+                nums2.Add(n);
+            }
+            return false;
+        }
+    }
+}
