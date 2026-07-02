bbd80c1b8ae034f5bedd8d15af6f31682ed59635
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-02 23:42:40 +0300

    feat: add Contains Duplicate Tests

---
+++ tests/LeetCode.Tests/LC0217_ContainsDuplicateTests.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0217_ContainsDuplicateTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 1 }, true)]
+        [InlineData(new int[] { 1, 2, 3, 4 }, false)]
+        [InlineData(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }, true)]
+        [InlineData(new int[] { 1 }, false)]
+        [InlineData(new int[] { }, false)]
+        public void ContainsDuplicate_ReturnsExpectedResult(int[] input, bool expected)
+        {
+            var solution = new LC0217_ContainsDuplicate();
+            var actual = solution.ContainsDuplicate(input);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
