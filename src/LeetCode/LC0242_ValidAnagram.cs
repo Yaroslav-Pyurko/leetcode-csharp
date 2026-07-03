ef1b4648149cd2c949408a706a0604a73e606bdf
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-03 19:29:38 +0300

    feat: add Valid Anagram and tests

---
+++ src/LeetCode/LC0242_ValidAnagram.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class LC0242_ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            var sortS = SortSring(s);
+            var sortT = SortSring(t);
+
+            if (String.Equals(sortS, sortT))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string SortSring(string s)
+        {
+            var charArray = s.ToLower().ToCharArray();
+            Array.Sort(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC0242_ValidAnagramTests.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0242_ValidAnagramTests
+    {
+        [Theory]
+        [InlineData("anagram", "nagaram", true)] 
+        [InlineData("rat",     "car",     false)]
+        public void IsAnagram_ReturnsExpectedResult(string inputS, string inputT, bool expected)
+        {
+            var solution = new LC0242_ValidAnagram();
+            var actual = solution.IsAnagram(inputS, inputT);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
