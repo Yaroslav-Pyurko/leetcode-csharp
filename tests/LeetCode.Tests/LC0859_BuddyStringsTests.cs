e87dcd7a8def9875d612e3819fb1e3414566c714
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-08 20:23:22 +0300

    feat: add Buddy Strings Tests

---
+++ tests/LeetCode.Tests/LC0859_BuddyStringsTests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0859_BuddyStringsTests
+    {
+        [Theory]
+        [InlineData("ab", "ba", true)]
+        [InlineData("ab", "ab", false)]
+        [InlineData("aa", "aa", true)]
+        [InlineData("longtextreallylongtext", "longtextreallylongtetx", true)]
+        [InlineData("longtextreallylongtext", "longtextreallylongtxte", false)]
+        public void BuddyStrings_ReturnsExpectedResult(string inputS, string inputT, bool expected)
+        {
+            var solution = new LC0859_BuddyStrings();
+
+            var actual = solution.BuddyStrings(inputS, inputT);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
