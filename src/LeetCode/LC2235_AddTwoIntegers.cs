20d9908df0deb82e79cc6149ea52d93363789efc
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-09 15:45:20 +0300

    feat: Add Two Integers and tests

---
+++ src/LeetCode/LC2235_AddTwoIntegers.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode
+{
+    public class LC2235_AddTwoIntegers
+    {
+        public int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC2235_AddTwoIntegersTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    public class LC2235_AddTwoIntegersTests
+    {
+        [Theory]
+        [InlineData(12, 5, 17)]
+        [InlineData(-10, 4, -6)]
+        public void Sum_ReturnsExpectedResult(int input1, int input2, int expected)
+        {
+            var solution = new LC2235_AddTwoIntegers();
+
+            var actual = solution.Sum(input1, input2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
