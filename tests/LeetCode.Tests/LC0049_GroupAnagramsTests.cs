5c62b60666d887a6cb140e267b3aa124e7c311b1
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-13 23:47:16 +0300

    feat: add Group Anagrams tests

---
+++ tests/LeetCode.Tests/LC0049_GroupAnagramsTests.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Tests
+{
+    public class LC0049_GroupAnagramsTests
+    {
+        [Theory]
+        [InlineData(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" }, "bat", "nat,tan", "ate,eat,tea")]
+        [InlineData(new string[] { "" }, "")]
+        [InlineData(new string[] { "a" }, "a")]
+        public void Test_WithFlattenedData(string[] inputArray, params string[] expectedGroups)
+        {
+            List<List<string>> expected = expectedGroups
+                .Select(group => group
+                    .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                    .ToList())
+                .ToList();
+            var solution = new LC0049_GroupAnagrams();
+
+            var actual = solution.GroupAnagrams(inputArray);
+
+            Assert.Equal(expected, actual);
+
+            var normalizedExpected = expected
+                .Select(group => group.OrderBy(word => word).ToList())
+                .OrderBy(group => group.FirstOrDefault() ?? string.Empty)
+                .ToList();
+
+            var normalizedActual = actual
+                .Select(group => group.OrderBy(word => word).ToList())
+                .OrderBy(group => group.FirstOrDefault() ?? string.Empty)
+                .ToList();
+
+            Assert.Equal(normalizedExpected, normalizedActual);
+        }
+    }
+}
