e97113e092d9cab316c2e7adfcaad973f149bdbe
Yaroslav Pyurko <yarickpy@gmail.com>
2026-07-24 23:37:36 +0300

    feat: add tests for Merge Two Sorted Lists

---
+++ src/LeetCode/Common/LinkedListBuilder.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Common
+{
+    public static class LinkedListBuilder
+    {
+        // Converts an array into a linked list of ListNode
+        public static ListNode? ToListNode(int[] values)
+        {
+            ListNode? head = null;
+            ListNode? tail = null;
+
+            foreach (var value in values)
+            {
+                var node = new ListNode(value);
+
+                if (head is null)
+                {
+                    head = node;
+                }
+                else
+                {
+                    tail!.next = node;
+                }
+
+                tail = node;
+            }
+
+            return head;
+        }
+
+        // Converts a linked list back into an array for comparison
+        public static int[] ToArray(ListNode? head)
+        {
+            var result = new List<int>();
+
+            while (head is not null)
+            {
+                result.Add(head.val);
+                head = head.next;
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/LC0021_MergeTwoSortedListsTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Tests
+{
+    public class LC0021_MergeTwoSortedListsTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
+        [InlineData(new int[] { }, new int[] { }, new int[] { })]
+        [InlineData(new int[] { }, new int[] { 0 }, new int[] { 0 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4 }, 
+            new int[] { 5, 6, 7, 8 }, 
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void MergeTwoLists_ReturnsExpectedResult(int[] list1, int[] list2, int[] expected)
+        {
+            var listNode1 = LinkedListBuilder.ToListNode(list1);
+            var listNode2 = LinkedListBuilder.ToListNode(list2);
+            var solution = new LC0021_MergeTwoSortedLists();
+
+            var actual = solution.MergeTwoLists(listNode1, listNode2);
+
+            Assert.Equal(expected, LinkedListBuilder.ToArray(actual));
+        }
+    }
+}
